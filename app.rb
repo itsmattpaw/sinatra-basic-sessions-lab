4266adb67fbf3613d252f43affa3db7d36ad8ce9
itsmattpaw <itsmattpaw@gmail.com>
2022-04-16 16:49:36 -0400

    commit tree 4546f61fdb41d325f03f0d6dd0a186ef9dcbf15a

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, Sysrandom.hex(64)
+    end
+
+    get '/' do
+        @session = session
+        erb :index
+    end
+    post '/checkout' do
+        @session = session
+        @session[:item] = params[:item]
+    end
 end
